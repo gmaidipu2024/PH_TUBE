eb1a57240b2d9d55649f3d014cfcb5a8ca20c048
Goutam Adhikari Dipu <gtm10322@gmail.com>
2025-03-12 17:32:03 +0600

    dyanamic btn add by api

---
+++ script/index.js
@@ -0,0 +1,22 @@
+ function loadCategories() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then ((res)=> res.json())
+    .then((data)=>displayCategories(data.categories))
+}
+    function displayCategories(categories){
+        // get container
+    const categoryContainer = document.getElementById("category-container");
+    for (let cat of categories ){
+        console.log(cat);
+        // crateElement
+        const category= document.createElement("div")
+        category.innerHTML= `
+        <button class="btn btn-sm"> ${cat.category}</button>
+            `
+        // append Element 
+        categoryContainer.append(category);
+    }
+
+    }
+
+loadCategories()
