d2987d02732bb3704b397da85f4b17021f192497
Goutam Adhikari Dipu <gtm10322@gmail.com>
2025-03-12 21:03:43 +0600

    video card add sucessfully

--- script/index.js
+++ script/index.js
@@ -52,19 +52,39 @@ const displayVideos = (videos) => {
     const videoCard = document.createElement("div");
     videoCard.innerHTML = `
 
-    <div class="card bg-base-100  shadow-sm">
-    <figure>
-        <img
-        src="${video.thumbnail}"
-        alt="Shoes" />
-    </figure>
-    <div class="card-body">
-        <h2 class="card-title">${video.title}</h2>
-        <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-        <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+    <div class="card bg-base-100  s">
+        <figure class="relative">
+            <img class= "w-full h-[150px] object-cover"
+
+            src="${video.thumbnail}"
+            alt="Shoes" />
+            <span class="absolute bottom-2 right-2 text-white bg-black px-2 rounded-md text-sm">
+                3hrs 56 min ago
+            </span>
+        </figure>
+
+        <div class=" flex gap-5  py-5"> 
+            <div>
+                
+                <div class="avatar">
+                    <div class="ring-primary ring-offset-base-100 w-6 px-0 rounded-full ring ring-offset-2">
+                        <img src="${video.authors[0].profile_picture}" alt="">
+                    </div>
+                  </div>
+            </div>
+            <div>
+                <h2>Building a Winning </h2>
+                <p class="text-sm text-gray-800 flex gap-1">${video.authors[0].profile_name}
+
+                    <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""> 
+                    
+                    </p>
+
+                    <p class="text-sm text-gray-800" >${video.others.views} views</p>
+                
+            </div>
+            
         </div>
-    </div>
     </div>
             `;
     videoContainer.append(videoCard);
